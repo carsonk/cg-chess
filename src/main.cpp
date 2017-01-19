8f06791dfe85b9e8fee74b1236d0e8305dedd35a
njankowski <njankowski@users.noreply.github.com>
2017-01-19 10:40:39 -0500

    Add SDL2 library.
    - Add SDL2 to solution include and library directories.
    - Add SDL2 as linker input.
    - Set linker subsystem to console.
    - Add post-build event to copy SDL2 runtime DLL to build directory.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,50 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include "SDL.h"
 #include "main.h"
-
 #include "Stockfish\src\position.h"
 
-int main() {
-    Position::init();
-    return 0;
+int main(int argc, char *argv[])
+{
+
+	Position::init();
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL_Init", SDL_GetError(), NULL);
+
+		return EXIT_FAILURE;
+	}
+
+	// SDL test initialization.
+
+	SDL_Window *sdlWindow = SDL_CreateWindow("cg-chess", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+
+	if (sdlWindow == NULL)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL_CreateWindow", SDL_GetError(), NULL);
+
+		SDL_Quit();
+
+		return EXIT_FAILURE;
+	}
+
+	bool isRunning = true;
+	while (isRunning)
+	{
+		SDL_Event sdlEvent;
+		while (SDL_PollEvent(&sdlEvent) != 0)
+		{
+			switch (sdlEvent.type)
+			{
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			}
+		}
+	}
+
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
