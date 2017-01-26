9ef20eb7d676dc9d3c98cd60fb78952bcdc56f6c
njankowski <njankowski@users.noreply.github.com>
2017-01-26 16:33:54 -0500

    Embed chess piece SVG assets into source files.

---
+++ src/vectorart.h
@@ -0,0 +1,40 @@
+/*
+	Copyright 2017, Nicholas Jankowski, Carson Killbreath, Nathan Oles,
+	Richard Peterson, Rebecca Roughton, Benjamin Schnell
+
+	This file is part of cg-chess.
+
+	cg-chess is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	cg-chess is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with cg-chess.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+
+extern const char *darkKingSVG;
+extern const char *lightKingSVG;
+
+extern const char *darkQueenSVG;
+extern const char *lightQueenSVG;
+
+extern const char *darkRookSVG;
+extern const char *lightRookSVG;
+
+extern const char *darkBishopSVG;
+extern const char *lightBishopSVG;
+
+extern const char *darkKnightSVG;
+extern const char *lightKnightSVG;
+
+extern const char *darkPawnSVG;
+extern const char *lightPawnSVG;
