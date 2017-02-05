8dc828abbb2eed4a29ce436a7968051bfc8e298a
njankowski <njankowski@users.noreply.github.com>
2017-02-05 17:36:19 -0500

    Add stub files.

---
+++ src/asset.cpp
@@ -0,0 +1,33 @@
+/*
+    Copyright 2017, Nicholas Jankowski, Carson Killbreath, Nathan Oles,
+    Richard Peterson, Rebecca Roughton, Benjamin Schnell
+
+    This file is part of cg-chess.
+
+    cg-chess is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cg-chess is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cg-chess.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "asset.h"
+
+
+bool Asset_Init(void)
+{
+    return true;
+}
+
+
+void Asset_Quit(void)
+{
+
+}
