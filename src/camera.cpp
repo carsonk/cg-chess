17158b0d925a678c755a329a2411ed2bfadc7c33
njankowski <njankowski@users.noreply.github.com>
2017-02-05 23:31:34 -0500

    Add notes about camera implementation.

--- src/camera.cpp
+++ src/camera.cpp
@@ -18,6 +18,50 @@
     along with cg-chess.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+    Camera Notes:
+
+    The camera is responsible for transforming the world into view, and applying
+    either a perspective or orthographic projection transformation.
+
+    Thus, the camera owns the View matrix and the Projection matrix.
+
+    It should manage these matrices using the GLM library.
+
+    When requested, it should set up the ModelView and Projection matrices
+    according to its separately stored View and Projection matrices.
+
+    The idea is that the Rendering subsystem will request that the camera set
+    up the ModelView and Projection matrices, and will then perform its own
+    transformations upon the ModelView matrix to achieve its independent goals.
+
+    The camera should also have input processing in its logic stage.
+
+    User input should affect both the View and Projection matrices.
+
+    Keyboard, mouse, and game controller are possible input sources to be
+    considered.
+
+    Camera movement should be as smooth as possible.
+
+    This may require that the camera implement an additional function and
+    storage for View matrix interpolation prior to rendering.
+
+    e.x.
+    Camera_Interpolate(uint32_t currentTick, double interpolation)
+
+    The camera will have a position, a looking direction, camera position
+    velocity, and looking direction velocity.
+
+    These may be implemented using GLM, which has support for vectors.
+
+    Implementing interpolation will require that the camera has a logical
+    position, interpolated position, logical looking direction, and interpolated
+    looking direction.
+
+*/
+
+
 #include "camera.h"
 
 
