8b5d7e996febcadbbcd4f71470dd3d18a4f46d59
njankowski <njankowski@users.noreply.github.com>
2017-02-05 23:43:12 -0500

    Add notes about game input implementation.

--- src/input.cpp
+++ src/input.cpp
@@ -18,6 +18,47 @@
     along with cg-chess.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+    Game Input Notes:
+
+    2D Mode
+        1. Calculate the Tile when a Click Occurs
+            Matrix transformations will occur in reverse order, mapping a window
+            coordinate to a world coordinate.
+            This must be done twice, one mapping for the near-plane, and one
+            mapping for the far-plane.
+            These two points form a line.
+            Using line/plane intersection, it will be determined what tile was
+            clicked upon.
+            In this mode, only the tile clicked on matters, not the piece.
+            Each tile either has a piece occupying it, or it doesn't.
+            Given the tile, the game logic will implicitly know the selected
+            piece.
+    3D Mode
+        1. Calculate the Tile and Piece when a Click Occurs
+            Matrix transformations will occur in reverse order, mapping a window
+            coordinate to a world coordinate.
+            This must be done twice, one mapping for the near-plane, and one
+            mapping for the far-plane.
+            These two points form a line.
+            Using line/plane intersection, it will be determined what tile and
+            what piece was clicked upon.
+            The game logic will choose what to do given the selected tile and
+            selected piece.
+            e.x. If no piece is selected, the game will prioritize the newly
+            clicked upon piece over the newly clicked upon tile.
+            (User selecting a piece.)
+            If a piece is already selected, the game will prioritize the newly
+            clicked upon tile over the newly clicked upon piece.
+            (User attempting to move a piece.)
+
+    Picking may be implemented separately in utility functions.
+    The renderer may need to transform chess piece bounding boxes into world
+    coordinates, so as to facilitate picking.
+
+*/
+
+
 #include "input.h"
 
 
