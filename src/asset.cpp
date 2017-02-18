7abe56b8b0e6577ff2a1cf7ae0d00b2490f97c14
njankowski <njankowski@users.noreply.github.com>
2017-02-18 11:40:48 -0500

    Load SVG assets from asset archive.

--- src/asset.cpp
+++ src/asset.cpp
@@ -18,16 +18,154 @@
     along with cg-chess.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <string>
+
 #include "asset.h"
+#include "SDL.h"
+#include "ZipFile.h"
+
+
+#define PIECE_SVG_PAWN_DARK "svg/Chess_pdt45.svg"
+#define PIECE_SVG_ROOK_DARK "svg/Chess_rdt45.svg"
+#define PIECE_SVG_KNIGHT_DARK "svg/Chess_ndt45.svg"
+#define PIECE_SVG_BISHOP_DARK "svg/Chess_bdt45.svg"
+#define PIECE_SVG_QUEEN_DARK "svg/Chess_qdt45.svg"
+#define PIECE_SVG_KING_DARK "svg/Chess_kdt45.svg"
+
+#define PIECE_SVG_PAWN_LIGHT "svg/Chess_plt45.svg"
+#define PIECE_SVG_ROOK_LIGHT "svg/Chess_rlt45.svg"
+#define PIECE_SVG_KNIGHT_LIGHT "svg/Chess_nlt45.svg"
+#define PIECE_SVG_BISHOP_LIGHT "svg/Chess_blt45.svg"
+#define PIECE_SVG_QUEEN_LIGHT "svg/Chess_qlt45.svg"
+#define PIECE_SVG_KING_LIGHT "svg/Chess_klt45.svg"
+
+
+const char *svgPawnDark;
+const char *svgRookDark;
+const char *svgKnightDark;
+const char *svgBishopDark;
+const char *svgQueenDark;
+const char *svgKingDark;
+
+const char *svgPawnLight;
+const char *svgRookLight;
+const char *svgKnightLight;
+const char *svgBishopLight;
+const char *svgQueenLight;
+const char *svgKingLight;
+
+
+void ReadLinesAndCopy(std::shared_ptr<ZipArchiveEntry> archiveEntry, const char *destination)
+{
+    // http://stackoverflow.com/questions/3203452/how-to-read-entire-stream-into-a-stdstring
+    std::istream *decompressionStream = archiveEntry->GetDecompressionStream();
+    std::string allLines(std::istreambuf_iterator<char>(*decompressionStream), {});
+
+    // http://stackoverflow.com/questions/347949/how-to-convert-a-stdstring-to-const-char-or-char
+    char *copiedString = new char[allLines.length() + 1];
+    strcpy_s(copiedString, allLines.length() + 1, allLines.c_str());
+
+    destination = copiedString;
+}
+
+
+// Asset_Quit() should free copied strings.
+bool LoadSVGAssets(std::shared_ptr<ZipArchive> archive)
+{
+    // Dark Pieces
+    ZipArchiveEntry::Ptr archiveEntry = archive->GetEntry(PIECE_SVG_PAWN_DARK);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgPawnDark);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_ROOK_DARK);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgRookDark);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_KNIGHT_DARK);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgKnightDark);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_BISHOP_DARK);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgBishopDark);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_QUEEN_DARK);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgQueenDark);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_KING_DARK);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgKingDark);
+
+    // Light Pieces
+    archiveEntry = archive->GetEntry(PIECE_SVG_PAWN_LIGHT);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgPawnLight);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_ROOK_LIGHT);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgRookLight);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_KNIGHT_LIGHT);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgKnightLight);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_BISHOP_LIGHT);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgBishopLight);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_QUEEN_LIGHT);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgQueenLight);
+
+    archiveEntry = archive->GetEntry(PIECE_SVG_KING_LIGHT);
+    if (archiveEntry == nullptr)
+        return false;
+    ReadLinesAndCopy(archiveEntry, svgQueenLight);
+
+    return true;
+}
 
 
 bool Asset_Init(void)
 {
+    ZipArchive::Ptr archive = ZipFile::Open("assets");
+
+    // Load SVG Assets
+    if (!LoadSVGAssets(archive))
+    {
+        return false;
+    }
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Asset_Init: Loaded SVG assets.");
+
     return true;
 }
 
 
 void Asset_Quit(void)
 {
+    delete(svgPawnDark);
+    delete(svgRookDark);
+    delete(svgKnightDark);
+    delete(svgBishopDark);
+    delete(svgQueenDark);
+    delete(svgKingDark);
 
+    delete(svgPawnLight);
+    delete(svgRookLight);
+    delete(svgKnightLight);
+    delete(svgBishopLight);
+    delete(svgQueenLight);
+    delete(svgKingLight);
 }
